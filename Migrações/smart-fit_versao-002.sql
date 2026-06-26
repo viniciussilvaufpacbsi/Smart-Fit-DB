fa6c169c20008ae2041546d542894850dea39533
Vinicius <vinicius.silva.silva@icen.ufpa.br>
2026-06-26 16:38:15 -0300

    Terceira Versão

---
+++ Migrações/smart-fit_versao-002.sql
@@ -0,0 +1,13 @@
+ALTER TABLE TURMA ADD CONSTRAINT Check_Turma_Turno CHECK(Turno IN ('Manhã','Tarde','Noite'));
+
+ALTER TABLE Plano_Assinatura ADD CONSTRAINT Check_Nome_Plano CHECK(Nome_Plano IN ('Smart','Fit','Black'));
+
+ALTER TABLE Pagamento ADD CONSTRAINT Check_Forma_Pagamento CHECK(Forma_Pagamento IN ('Dinheiro','Pix','Crédito','Débito'));
+
+ALTER TABLE Equipamento ADD CONSTRAINT Check_Condicao_Equipamento CHECK (Condicao_Equipamento IN ('Excelente','Bom','Ruim'));
+
+ALTER TABLE Instrutor ADD CONSTRAINT Check_Especialidade_Instrutor CHECK (Especialidade IN (
+'Yoga','Pilates','Spinning','Zumba',
+'Musculação','Hipertrofia','Emagrecimento','Fortalecimento','Condicionamento','Alongamento e Flexibilidade',
+'Karatê','Jiu-jitsu','Capoeira','Kung Fu','Taekwondo'));
+
