fe044e7de804cb32253468facf92536fbf2d5ca7
Vinicius <vinicius.silva.silva@icen.ufpa.br>
2026-06-27 17:38:58 -0300

    Sétima Versão
    Adição de constraints UNIQUE para as tabelas "Cliente" e "Instrutor"

---
+++ Migrações/smart-fit_versao-006.sql
@@ -0,0 +1,5 @@
+--                                      SEXTA MIGRAÇÃO
+
+ALTER TABLE Cliente ADD CONSTRAINT Unique_Telefone_Cliente UNIQUE (Telefone_Contato);
+
+ALTER TABLE Instrutor ADD CONSTRAINT Unique_Telefone_Instrutor UNIQUE(Telefone_Contato);
