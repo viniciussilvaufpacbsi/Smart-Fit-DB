32a75bf6d162263bb0e8ac74129efa73a7f0f766
Vinicius <vinicius.silva.silva@icen.ufpa.br>
2026-06-27 18:40:13 -0300

    UPDATE smart-fit_versao-004.sql

--- Migrações/smart-fit_versao-004.sql
+++ Migrações/smart-fit_versao-004.sql
@@ -1,4 +1,5 @@
 --                                        QUARTA MIGRAÇÃO
+
 ALTER TABLE Plano_Exercicios RENAME COLUMN Tipo_Exercicio TO Modalidade_Exercicio;
 
 ALTER TABLE Plano_Exercicios RENAME COLUMN Periodo_Exercicio TO Duracao_Exercicio;
