d100554513fcb27fcc389b7d52a1df757e77c34d
Vinicius <vinicius.silva.silva@icen.ufpa.br>
2026-06-27 17:08:17 -0300

    DELETE Migrações/smart-fit_versao-004.sql
    Arquivo duplicado

--- Migrações/smart-fit_versao-004.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE Plano_Exercicios RENAME COLUMN Tipo_Exercicio TO Modalidade_Exercicio;
-
-ALTER TABLE Plano_Exercicios RENAME COLUMN Periodo_Exercicios TO Duracao_Exercicio;
-
-ALTER TABLE Plano_Exercicios ADD CONSTRAINT Check_Modalidade_Exercicio CHECK (Modalidade_Exercicio IN (
- 'FITDANCE','ZUMBA','BODY COMBAT','BODY BALANCE','SMART CORE',
- 'ALONGAMENTO','SMART FIGHT','SMART BIKE'));
