290a21225c22bfb379f48ecb4a4769bab4604ff9
Vinicius <vinicius.silva.silva@icen.ufpa.br>
2026-06-29 08:45:56 -0300

    UPDATE smart-fit_versao-002.sql

--- Migrações/smart-fit_versao-002.sql
+++ Migrações/smart-fit_versao-002.sql
@@ -1,9 +1,5 @@
 --                                                SEGUNDA MIGRAÇÃO
-CREATE TABLE Plano_Exercicios (
-  ID_Plano SERIAL PRIMARY KEY,
-  Tipo_Exercicio VARCHAR(200) NOT NULL,
-  Quantidade_Exercicios INT NOT NULL,
-  Periodo_Exercicios VARCHAR(20) NOT NULL,
-  Turma_ID INT REFERENCES Turma(ID_Turma) ON DELETE CASCADE,
-  Instrutor_ID VARCHAR(6) REFERENCES Instrutor(CREF) ON DELETE CASCADE
-);
+
+ALTER TABLE Plano_Exercicios RENAME COLUMN Periodo_Exercicio TO Duracao_Exercicio;
+
+ALTER TABLE Plano_Exercicios RENAME COLUMN Tipo_Exercicio TO Modalidade_Exercicio;
