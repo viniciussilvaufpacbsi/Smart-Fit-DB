c9696de06184ad6da5a8363af0a6f67db32cb98d
Vinicius <vinicius.silva.silva@icen.ufpa.br>
2026-06-29 08:46:25 -0300

    UPDATE smart-fit_versao-004.sql

--- Migrações/smart-fit_versao-004.sql
+++ Migrações/smart-fit_versao-004.sql
@@ -1,9 +1,5 @@
 --                                        QUARTA MIGRAÇÃO
 
-ALTER TABLE Plano_Exercicios RENAME COLUMN Tipo_Exercicio TO Modalidade_Exercicio;
-
-ALTER TABLE Plano_Exercicios RENAME COLUMN Periodo_Exercicio TO Duracao_Exercicio;
-
 ALTER TABLE Plano_Exercicios ADD CONSTRAINT Check_Modalidade_Exercicio CHECK (Modalidade_Exercicio IN (
  'FITDANCE','ZUMBA','BODY COMBAT','BODY BALANCE','SMART CORE',
  'ALONGAMENTO','SMART FIGHT','SMART BIKE'));
