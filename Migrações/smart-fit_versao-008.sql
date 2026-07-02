1625a849a8c0c5f2f4defafc7fe4e1590466a478
Vinicius <vinicius.silva.silva@icen.ufpa.br>
2026-07-02 09:22:32 -0300

    UPDATE smart-fit_versao-008.sql

--- Migrações/smart-fit_versao-008.sql
+++ Migrações/smart-fit_versao-008.sql
@@ -1,6 +1,6 @@
 --                                    OITAVA MIGRAÇÃO          
 
-ALTER TABLE Equipamento ADD COLUMN Academia_ID VARCHAR(14) REFERENCES Academia(CNPJ);
+ALTER TABLE Equipamento ADD COLUMN Academia_ID VARCHAR(14) REFERENCES Academia(CNPJ) ON DELETE CASCADE;
 
 ALTER TABLE Cliente DROP COLUMN Turma_ID;
 
