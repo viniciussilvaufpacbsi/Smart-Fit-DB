b5774afb13646d829fb970478a2b0298179affd3
Vinicius <vinicius.silva.silva@icen.ufpa.br>
2026-07-02 20:05:51 -0300

    UPDATE and RENAME smart-fit_versao-007.sql

--- Migrações/smart-fit_migration_007.sql
+++ Migrações/smart-fit_migration_007.sql
@@ -1,5 +1,3 @@
---                                          SÉTIMA MIGRAÇÃO
-
 ALTER TABLE Cliente ADD CONSTRAINT Unique_Telefone_Cliente UNIQUE (Telefone_Contato);
 
 ALTER TABLE Instrutor ADD CONSTRAINT Unique_Telefone_Instrutor UNIQUE(Telefone_Contato);
